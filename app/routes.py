2fedefc84b94e3a8f0855253cd37c784512f3de7
Julia <hiljaotsola2766@gmail.com>
2022-12-08 02:15:41 +0300

    Add url_for insted of hardcoded links to base.html and login view

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,5 @@
 from app import app
-from flask import render_template, flash, redirect
+from flask import render_template, flash, redirect, url_for
 from app import app
 from app.forms import LoginForm
 
@@ -29,7 +29,7 @@ def login():
     if form.validate_on_submit():
         flash(f'Login requested for user {form.username.data},'
         f'remember_me={form.remember_me.data}')
-        return redirect('/index')
+        return redirect(url_for('index'))
     return render_template(
         'login.html', title='Sign In',
         form=form)
