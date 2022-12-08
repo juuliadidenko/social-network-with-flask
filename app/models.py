3bfdeeac015c8b1deb155927f87b9df411ecb440
Julia <hiljaotsola2766@gmail.com>
2022-12-09 01:28:41 +0300

    Add user avatar feature:
    Create avatar method in User, update user template

--- app/models.py
+++ app/models.py
@@ -1,5 +1,6 @@
 from app import db, login
 from datetime import datetime
+from hashlib import md5
 from werkzeug.security import (generate_password_hash,
     check_password_hash)
 from flask_login import UserMixin
@@ -20,6 +21,11 @@ class User(UserMixin, db.Model):
 
     def check_password(self, password):
         return check_password_hash(self.password_hash, password)
+
+    def avatar(self, size):
+        digest = md5(self.email.lower().encode('utf-8')).hexdigest()
+        url = 'https://www.gravatar.com/avatar/'
+        return f'{url}{digest}?d=identicon&s={size}'
 
 
 class Post(db.Model):
