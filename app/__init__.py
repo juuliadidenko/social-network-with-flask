95b584117f26f5556baee478ecbfe7b781dd97b0
Julia <hiljaotsola2766@gmail.com>
2022-12-12 00:34:44 +0300

    Create custom cli commands for translation

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,9 @@
 import logging
 from logging.handlers import SMTPHandler, RotatingFileHandler
 import os
+import click
 from flask import Flask, request
+from flask.cli import AppGroup
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
@@ -23,6 +25,43 @@ mail = Mail(app)
 bootstrap = Bootstrap(app)
 moment = Moment(app)
 babel = Babel(app)
+translate = AppGroup('translate')
+
+
+@translate.command('init')
+@click.argument('lang')
+def init(lang):
+    """Initialize a new language."""
+    if os.system(
+        'pybabel extract -F babel.cfg -k _l -o messages.pot .'):
+        raise RuntimeError('extract command failed')
+    if os.system(
+            'pybabel init -i messages.pot -d app/translations -l '
+            + lang):
+        raise RuntimeError('init command failed')
+    os.remove('messages.pot')
+
+
+@translate.command('update')
+def update():
+    """Update all languages."""
+    if os.system(
+        'pybabel extract -F babel.cfg -k _l -o messages.pot .'):
+        raise RuntimeError('extract command failed')
+    if os.system(
+        'pybabel update -i messages.pot -d app/translations'):
+        raise RuntimeError('update command failed')
+    os.remove('messages.pot')
+
+
+@translate.command('compile')
+def compile():
+    """Compile all languages."""
+    if os.system('pybabel compile -d app/translations'):
+        raise RuntimeError('compile command failed')
+
+
+app.cli.add_command(translate)
 
 
 if not app.debug:
