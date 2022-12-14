bdca6e96ddf1e3851cacc516546a940ddfc7be2a
Julia <hiljaotsola2766@gmail.com>
2022-12-15 01:59:18 +0300

    Connect app to  postgresql database, update config.py and tests.py

--- config.py
+++ config.py
@@ -2,14 +2,14 @@ import os
 from dotenv import load_dotenv
 
 
-load_dotenv()
-
 basedir = os.path.abspath(os.path.dirname(__file__))
+load_dotenv(os.path.join(basedir, '.env'))
+
 
 class Config(object):
     SECRET_KEY = os.getenv('SECRET_KEY')
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL') or \
-        'sqlite:///' + os.path.join(basedir, 'app.db')
+    CSRF_ENABLED = True
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     MAIL_SERVER = os.getenv('MAIL_SERVER')
     MAIL_PORT = int(os.getenv('MAIL_PORT'))
--- tests.py
+++ tests.py
@@ -1,47 +1,42 @@
 from datetime import datetime, timedelta
 import unittest
-from app import app, db
+from app import create_app, db
 from app.models import User, Post
+from config import Config
+
+
+class TestConfig(Config):
+    TESTING = True
+    SQLALCHEMY_DATABASE_URI = 'sqlite://'
 
 
 class UserModelCase(unittest.TestCase):
     def setUp(self):
-        app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///'
-        app_ctxt = app.app_context()
-        app_ctxt.push()
+        self.app = create_app(TestConfig)
+        self.app_context = self.app.app_context()
+        self.app_context.push()
         db.create_all()
-        app_ctxt.pop()
 
     def tearDown(self):
-        app_ctxt = app.app_context()
-        app_ctxt.push()
         db.session.remove()
         db.drop_all()
-        app_ctxt.pop()
+        self.app_context.pop()
 
     def test_password_hashing(self):
-        app_ctxt = app.app_context()
-        app_ctxt.push()
         u = User(username='susan')
         u.set_password('cat')
         self.assertFalse(u.check_password('dog'))
         self.assertTrue(u.check_password('cat'))
-        app_ctxt.pop()
 
     def test_avatar(self):
-        app_ctxt = app.app_context()
-        app_ctxt.push()
         u = User(username='john', email='john@example.com')
         self.assertEqual(
             u.avatar(128),
             ('https://www.gravatar.com/avatar/'
             'd4c74594d841139328695756648b6bd6'
             '?d=identicon&s=128'))
-        app_ctxt.pop()
 
     def test_follow(self):
-        app_ctxt = app.app_context()
-        app_ctxt.push()
         u1 = User(username='john', email='john@example.com')
         u2 = User(username='susan', email='susan@example.com')
         db.session.add(u1)
@@ -61,11 +56,8 @@ class UserModelCase(unittest.TestCase):
         self.assertFalse(u1.is_following(u2))
         self.assertEqual(u1.followed.count(), 0)
         self.assertEqual(u2.followers.count(), 0)
-        app_ctxt.pop()
 
     def test_follow_posts(self):
-        app_ctxt = app.app_context()
-        app_ctxt.push()
         u1 = User(username='john', email='john@example.com')
         u2 = User(username='susan', email='susan@example.com')
         u3 = User(username='mary', email='mary@example.com')
@@ -106,7 +98,6 @@ class UserModelCase(unittest.TestCase):
         self.assertEqual(f2, [p2, p3])
         self.assertEqual(f3, [p3, p4])
         self.assertEqual(f4, [p4])
-        app_ctxt.pop()
 
 
 if __name__ == "__main__":
